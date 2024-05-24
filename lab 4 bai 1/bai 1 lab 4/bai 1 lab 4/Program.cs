98ea9067cb9665fa104bb515075256d099947af9
unknown <manhdao248205@gmail.com>
2024-05-24 22:19:07 +0700

    lab 4

---
+++ lab 4 bai 1/bai 1 lab 4/bai 1 lab 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_1_lab_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Lambda Expression để tính bình phương của một số
+            Func<int, int> square = x => x * x;
+            int result = square(5);
+            Console.WriteLine("Bình phương của 5 là: " + result);
+
+            // Lambda Expression để kiểm tra số chẵn
+            Predicate<int> isEven = x => x % 2 == 0;
+            Console.WriteLine("6 có phải là số chẵn không? " + isEven(6));
+
+            // Lambda Expression để in ra các phần tử trong danh sách
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Các số trong danh sách: ");
+            numbers.ForEach(num => Console.Write(num + " "));
+            Console.WriteLine();
+
+            // Lambda Expression để tính tổng các số chia hết cho 3 trong danh sách
+            var sumDivisibleBy3 = numbers.Where(num => num % 3 == 0).Sum();
+            Console.WriteLine("Tổng các số chia hết cho 3 là: " + sumDivisibleBy3);
+        }
+    }
+
+}
---
+++ lab 4 bai 1/bai 1 lab 4/bai 2 lab 4/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class userDataLab4
+    {
+        public string name;
+        public int level;
+
+        public userDataLab4(string _name, int lv)
+        {
+            name = _name;
+            level = lv;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<userDataLab4> list = new List<userDataLab4>();
+            list.Add(new userDataLab4("GTA 5", 10));
+            list.Add(new userDataLab4("Genshin impact", 15));
+            list.Add(new userDataLab4("Gunny", 5));
+            list.Add(new userDataLab4("Liên minh huyền thoại", 100));
+
+            var selectedData = list.Select(u => new { u.name, u.level });
+            foreach (var item in selectedData)
+            {
+                Console.WriteLine("Name: " + item.name + " | level: " + item.level);
+            }
+
+  
+            var sortedData = list.OrderByDescending(u => u.level).Select(u => new { u.name, u.level });
+            Console.WriteLine("\nDanh sách sắp xếp theo thứ tự giảm dần của level:");
+            foreach (var item in sortedData)
+            {
+                Console.WriteLine("Name: " + item.name + " | level: " + item.level);
+            }
+        }
+    }
+}
